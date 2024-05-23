7da31b91310a4eee39ce137ac7f8f75318da2530
LIU Kendong <kdliu2@ad.cityu.edu.hk>
2024-05-24 03:26:21 +0800

    Update1

--- static/js/clicklong.js
+++ static/js/clicklong.js
@@ -1,13 +1,13 @@
 function currentSlide(sliderIndex, slideIndex) {
-    const slider = document.getElementById(`sliderlong${sliderIndex}`);
-    const slides = slider.getElementsByClassName('slidelong');
-    const dots = slider.getElementsByClassName('dotlong');
+    const sliderlong = document.getElementById(`sliderlong${sliderIndex}`);
+    const slideslong = sliderlong.getElementsByClassName('slidelong');
+    const dotslong = sliderlong.getElementsByClassName('dotlong');
 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-        dots[i].classList.remove('active');
+    for (let i = 0; i < slideslong.length; i++) {
+        slideslong[i].style.display = 'none';
+        dotslong[i].classList.remove('active');
     }
 
-    slides[slideIndex - 1].style.display = 'block';
-    dots[slideIndex - 1].classList.add('active');
+    slideslong[slideIndex - 1].style.display = 'block';
+    dotslong[slideIndex - 1].classList.add('active');
 }
